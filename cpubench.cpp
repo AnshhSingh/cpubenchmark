d4b72ec37d5137a57a918d21b3de3a80212c67cc
AnshhSingh <anshsingh25bd@gmail.com>
2023-11-09 01:58:21 +0530

    added final score

--- cpubench.cpp
+++ cpubench.cpp
@@ -3,13 +3,17 @@
 #include <vector>
 #include <cmath>
 #include <thread>
+#include <intrin.h>
+
 using namespace std;
 using namespace chrono;
+
 void arithmetic();
 void memory();
 void floating_point();
 void vec();
-void threading();
+void threading(int);
+
 int main()
 {
 
@@ -21,19 +25,18 @@ int main()
     
     auto endTime = high_resolution_clock::now();
     auto duration = duration_cast<milliseconds>(endTime - startTime);
+cout << "Single Core score " << (float)duration.count()/4;
+int sc=duration.count();
+    threading(sc);
 
-    auto startTime2 = high_resolution_clock::now();
-    threading();
- auto endTime2 = high_resolution_clock::now();
-    auto duration2 = duration_cast<milliseconds>(endTime2 - startTime2);
-    cout << "Multi-threading: " << duration.count() << " ms\n";
+
 }
 void arithmetic()
 {
   
    
     auto startTime = high_resolution_clock::now();
-    const int numIterations = 1000000000; // Adjust the number of iterations as needed
+    const int numIterations = 2100000000; // Adjust the number of iterations as needed
     double result = 0.0;
     for (int i = 0; i < numIterations; ++i)
     {
@@ -47,7 +50,7 @@ void arithmetic()
 void memory()
 {
     auto startTime = high_resolution_clock::now();
-    const int arraySize = 100000000;
+    const int arraySize = 2000000000;
     int *dataArray = new int[arraySize];
 
     for (int i = 0; i < arraySize; ++i)
@@ -69,7 +72,7 @@ void floating_point()
 {
     auto startTime = high_resolution_clock::now();
     double result = 0.0;
-    const int numIterations = 200000000;
+    const int numIterations = 150000000;
     int i = 0;
     for (i = 0; i < numIterations; i++)
     {
@@ -83,7 +86,7 @@ void floating_point()
 void vec()
 {
     auto startTime = high_resolution_clock::now();
-    const int N = 10000; // Matrix size
+    const int N = 2000; // Matrix size
     std::vector<std::vector<double>> matrix1(N, std::vector<double>(N, 2.0));
     std::vector<std::vector<double>> matrix2(N, std::vector<double>(N, 3.0));
     std::vector<std::vector<double>> result(N, std::vector<double>(N, 0.0));
@@ -103,25 +106,38 @@ void vec()
     cout << "Vector: " << duration.count() << " ms\n";
     
 }
-void threading()
+void threading(int sc)
 {
     auto startTime = high_resolution_clock::now();
     
-   
+   streambuf *old = cout.rdbuf();
+cout.rdbuf(0);
      std::thread t2(arithmetic);
      std::thread t1(memory);
      std::thread t3(arithmetic);
      std::thread t4(memory);
      std::thread t5(floating_point);
      std::thread t6(vec);
+      std::thread t7(arithmetic);
+     std::thread t8(memory);
+     std::thread t9(arithmetic);
+     std::thread t10(memory);
     t1.join();
     t2.join();
     t3.join();
     t4.join();
     t5.join();
     t6.join();
+    t7.join();
+    t8.join();
+    t9.join();
+    t10.join();
+    
         auto endTime = high_resolution_clock::now();
     auto duration = duration_cast<milliseconds>(endTime - startTime);
-    cout << "Arithmetic: " << duration.count() << " ms\n";
+    cout.rdbuf(old);
+    cout << "\nMulti Core score " << (float)duration.count()/10;
+    cout<<"\nFinalScore(lower is better) - "<<((float)(sc/3)+(float)(duration.count()/1.5))/2 << endl;
+    
     
 }
